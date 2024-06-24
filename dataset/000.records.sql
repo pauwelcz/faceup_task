1a1a5ee1972f0bc035d2e0f7e2b533b1b4efeb18
pauwelcz <pavel.sedlar1993@gmail.com>
2024-06-24 20:23:57 +0200

    feat: add columns

--- dataset/000.records.sql
+++ dataset/000.records.sql
@@ -4,11 +4,13 @@ CREATE TABLE records (
     id SERIAL PRIMARY KEY, 
     name VARCHAR(255) NOT NULL,
     age INTEGER NOT NULL,
+    title VARCHAR(255),
+    note VARCHAR(255),
     created_at TIMESTAMP DEFAULT NOW() NOT NULL,
     updated_at TIMESTAMP
 );
 
-INSERT INTO records (name, age)
-VALUES ('pavelsedlar', 1), 
-       ('donaldtrump', 55), 
-       ('johndoe', 3);
+INSERT INTO records (id, name, age, title, note)
+VALUES (1, 'pavelsedlar', 1, 'test','lorem ipsum'), 
+       (2, 'donaldtrump', 55, 'truth', 'media are bad'), 
+       (3, 'johndoe', 3, NULL, NULL);
