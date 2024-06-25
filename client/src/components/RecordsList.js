9f40bc803fc1a3b26a5a7abfae17b0cc5d669d30
pauwelcz <pavel.sedlar1993@gmail.com>
2024-06-25 21:45:30 +0200

    fix: validation

--- client/src/components/RecordsList.js
+++ client/src/components/RecordsList.js
@@ -35,13 +35,17 @@ function RecordList() {
   const [limit] = useState(10);
   const [page, setPage] = useState(1);
 
-  const handleChange = (event, value) => {
-    setPage(value);
-  };
   const { loading, error, data, refetch } = useQuery(RECORDS_QUERY, { variables: { limit, offset: (page - 1) * limit}});
 
   if (loading) return 'Loading...';
   if (error) return `Error! ${error}`;
+
+  const handleChange = async (event, value) => {
+    setPage(value);
+    refetch({
+      offset: (value - 1) * limit
+    });
+  };
 
   return (
     <Paper style={{margin: 5, padding: 2, backgroundColor: "#97ebf0", border: 10, borderColor: "#FFF" }} >
