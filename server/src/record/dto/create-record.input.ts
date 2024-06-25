25f4b875886bc3aca44554001e01feaeef5f4244
pauwelcz <pavel.sedlar1993@gmail.com>
2024-06-25 09:42:54 +0200

    feat: add validation pipe

--- server/src/record/dto/create-record.input.ts
+++ server/src/record/dto/create-record.input.ts
@@ -1,16 +1,25 @@
 import { InputType, Field, Int } from '@nestjs/graphql';
+import { IsOptional, MaxLength, Min, MinLength } from 'class-validator';
 
 @InputType()
 export class CreateRecordInput {
-  @Field({ description: 'Example field (placeholder)' })
+  @Field({ description: 'Name of user' })
+  @MinLength(1)
+  @MaxLength(255)
   name: string;
 
+  @Min(1)
   @Field(() => Int, { description: 'Example field (placeholder)' })
   age: number;
 
+  @MinLength(1)
+  @MaxLength(255)
   @Field({ nullable: true, description: 'Example field (placeholder)' })
   title: string;
 
+  @MinLength(1)
+  @MaxLength(255)
+  @IsOptional()
   @Field({ nullable: true, description: 'Example field (placeholder)' })
   note: string;
 }
