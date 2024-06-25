0227141bd9f2723f3bbb947f400f759fed65faad
pauwelcz <pavel.sedlar1993@gmail.com>
2024-06-25 09:18:26 +0200

    fix: delete unnecessary decorators

--- server/src/record/entities/record.entity.ts
+++ server/src/record/entities/record.entity.ts
@@ -1,34 +1,31 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
-import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity('records')
-@ObjectType()
 export class Record {
   @PrimaryGeneratedColumn()
-  @Field(() => Int, { description: 'Example field (placeholder)' })
   id: number;
 
   @Column()
-  @Field({ description: 'Example field (placeholder)' })
   name: string;
 
   @Column()
-  @Field({ nullable: true, description: 'Example field (placeholder)' })
   title: string;
 
   @Column()
-  @Field({ nullable: true, description: 'Example field (placeholder)' })
   note: string;
 
   @Column()
-  @Field(() => Int, { description: 'Example field (placeholder)' })
   age: number;
 
   @CreateDateColumn()
-  @Field()
   created_at: Date;
 
   @UpdateDateColumn()
-  @Field({ nullable: true })
   updated_at: Date;
 }
