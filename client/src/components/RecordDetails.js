cb1e179c4544ff034a7f9f1b343358b681c27a1f
pauwelcz <pavel.sedlar1993@gmail.com>
2024-06-25 19:18:30 +0200

    feat:  show record details

---
+++ client/src/components/RecordDetails.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import Grid from '@mui/material/Grid';
+import { Dialog, DialogActions, DialogContent, DialogTitle, Paper } from '@mui/material';
+import Button from '@mui/material/Button';
+
+function RecordDetails(props) {
+  const {name, title, created_at, age, note } = props.record;
+
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return(
+    <Grid item xs={1}> 
+      <Button onClick={handleClickOpen}>Details</Button>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Record details</DialogTitle>
+        <DialogContent>
+          <Paper>
+            <Grid margin={1}>
+              <Paper>
+                <strong>{title}</strong>
+              </Paper>
+            </Grid>
+            <Grid margin={1}>
+              <Paper>
+                by <strong>{name}</strong>, age <strong>{age}</strong>
+              </Paper>
+            </Grid>
+            <Grid margin={1}>
+              <Paper>
+                Note
+              </Paper>
+            </Grid>
+            <Grid margin={1}>
+              <Paper>
+                {note}
+              </Paper>
+            </Grid>
+            <Grid margin={1}>
+              <Paper>
+                Created: {(new Date(created_at)).toUTCString()}
+              </Paper>
+            </Grid>
+          </Paper>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Close</Button>
+        </DialogActions>
+      </Dialog>
+    </Grid>
+  );
+}
+
+export default RecordDetails;
