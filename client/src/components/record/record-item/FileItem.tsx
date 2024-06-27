0d497ab2090b6910dcfba505283293d247097727
pauwelcz <pavel.sedlar1993@gmail.com>
2024-06-27 17:28:08 +0200

    fix: allignment

--- client/src/components/record/record-item/FileItem.tsx
+++ client/src/components/record/record-item/FileItem.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from 'react';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
-import { gridItemStyle, paperStyle } from '../../../styles';
+import { gridContainerStyle, gridItemStyle, paperStyle } from '../../../styles';
 import { File } from '../../../types/file-type';
 import FileNameFormatter from '../../utils/FileNameFormatter';
 import { Button } from '@mui/material';
@@ -20,14 +20,16 @@ const FileItem: FC<FileItemProps> = (props) => {
   const { id, filename, extension} = file;
 
   return (
-        <Paper style={{...paperStyle, backgroundColor: id % 2 === 0 ? '#afeaed' : '#97ebf0', borderColor: 'black' }}>
-          <Grid item xs={1} style={gridItemStyle} >
-            <Button variant='contained' startIcon={<DownloadIcon />} onClick={() => handleDownload(id)} />
-          </Grid>
-          <Grid item xs={1} style={gridItemStyle} >
-            <FileNameFormatter filename={filename} extension={extension} />
-          </Grid>
-        </Paper>
+    <Paper style={{...paperStyle, backgroundColor: id % 2 === 0 ? '#afeaed' : '#97ebf0', borderColor: 'black' }}>
+      <Grid container style={gridContainerStyle}>
+        <Grid item >
+          <Button startIcon={<DownloadIcon />} onClick={() => handleDownload(id)} />
+        </Grid>
+        <Grid item >
+          <FileNameFormatter filename={filename} extension={extension} />
+        </Grid>
+      </Grid>
+    </Paper>
   );
 }
 
