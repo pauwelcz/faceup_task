eef589fa3562783cb25e847baf26c27c195c10b9
pauwelcz <pavel.sedlar1993@gmail.com>
2024-06-27 22:26:21 +0200

    fix: attached files

--- client/src/components/record/record-item/AttachedFiles.tsx
+++ client/src/components/record/record-item/AttachedFiles.tsx
@@ -1,35 +1,24 @@
-import { useQuery } from '@apollo/client';
 import { Paper, Typography } from '@mui/material';
 import React, { FC } from 'react';
-import { FILES_BY_RECORD_QUERY } from '../../../graphql/graphqlOperations';
 import { File } from '../../../types/file-type';
 import FileItem from './FileItem';
 import { paperStyle } from '../../../styles';
 
 type AttachedFilesProps = {
-  id: number;
+  files: File[];
 }
 
-type FilesData = {
-  filesByRecord: {
-    totalNumber: number;
-    files: File[];
-  };
-};
-
 const AttachedFiles: FC<AttachedFilesProps> = (props) => {
-  const { id } = props;
-  const { loading, error, data } = useQuery<FilesData>(FILES_BY_RECORD_QUERY, { variables: { recordId: id }});
+  const { files } = props;
   return(<>
-    { data?.filesByRecord.totalNumber !== 0 && 
     <Paper style={{...paperStyle, backgroundColor: '#02ecfa'}}>
       <Typography padding={1}>
         <strong>Attached files:</strong>
       </Typography>
-      {data?.filesByRecord.files.map((file) => (
+      {files.map((file) => (
         <FileItem file={file} />
       ))}
-    </Paper>}
+    </Paper>
   </>);
 }
 
